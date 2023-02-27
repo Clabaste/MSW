cd3cffa1fe4ea637e917585e29ad79072a5c53a3
bastert <bastert@mediaworx.de>
2023-02-27 14:10:00 +0100

    setup service worker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import './app.css'
 import App from './App.svelte'
+import { worker } from './mocks/browser';
+if (import.meta.env.DEV) {
+  worker.start();
+}
 
 const app = new App({
   target: document.getElementById('app'),
