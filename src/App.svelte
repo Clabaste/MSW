520f01b02103d48186b2760767fcb31482cae43d
bastert <bastert@mediaworx.de>
2023-02-27 14:06:12 +0100

    use starwars api to create a mock call

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,29 @@
 <script>
+    import { onMount } from "svelte";
 
+    let promise = Promise.resolve([]);
+    async function fetchPlanets() {
+        const response = await self.fetch('https://www.swapi.tech/api/planets');
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(planets);
+        }
+    }
+    onMount(() => {
+        promise = fetchPlanets();
+    })
 </script>
 
 <main>
-
+    {#await promise}
+        <p>...waiting</p>
+    {:then planets}
+        <pre>{JSON.stringify(planets, null, 2)}</pre>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
 </main>
 
-<style>
 
-</style>
